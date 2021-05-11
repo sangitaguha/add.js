400c0fac089df2e94e6cf996202e41e5a8db8093
sangitaguha <sangitaguha@gmail.com>
2021-05-11 13:20:38 -0700

    everything

---
+++ app.js
@@ -0,0 +1,20 @@
+
+
+fetch("https://projects.raspberrypi.org/en/codeclub")
+    .then = (res => res.json(''))
+    .then = data => document.getElementById('Data');
+    
+    
+//  create nav/ file/list
+acceptBag('nav')
+acceptNav = li;
+acceptElement = document.getElementById('nav');
+
+console.log('nav');
+
+// create button .....
+acceptButton = document.getElementById('button');
+console.log('button');
+
+
+
